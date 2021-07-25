caf15ab39bcd4b7ec86be7269f53df91060a1bca
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-25 12:29:20 +0100

    Progressing

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -4,11 +4,23 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Time Tracker'),
-          centerTitle: true,
-          elevation: 2,
-        ),
-        body: Container(color: Colors.yellow));
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        centerTitle: true,
+        elevation: 2,
+      ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(children: <Widget>[
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              width: 100,
+              height: 100,
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
