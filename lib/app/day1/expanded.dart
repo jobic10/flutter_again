d7e85d1270cd6494312111739e183a3fdfede799
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-26 13:53:55 +0100

    WillPopScope

--- lib/app/day1/expanded.dart
+++ lib/app/day1/expanded.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_again/route/routeConstant.dart';
 
 class ExpandPage extends StatelessWidget {
   const ExpandPage({required this.data, Key? key}) : super(key: key);
@@ -6,29 +7,35 @@ class ExpandPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Working With Expanded'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              color: Colors.green,
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.of(context).pop('Data from Expanded');
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Working With Expanded'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.green,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.white,
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.white,
+              ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.green,
+            Expanded(
+              child: Container(
+                color: Colors.green,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
