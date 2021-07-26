7c6fe8e90d846efd0c6b654f13b99152c8b68c25
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-26 19:35:53 +0100

    Tried it again from scratch

--- lib/app/day1/expanded2.dart
+++ lib/app/day1/expanded2.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 
 class ExpandedAssignment extends StatelessWidget {
   const ExpandedAssignment({Key? key}) : super(key: key);
@@ -13,7 +13,7 @@ class ExpandedAssignment extends StatelessWidget {
         children: [
           _buildBox(color: Colors.red, flex: 8),
           Expanded(
-            flex: 5,
+            flex: 4,
             child: Row(
               children: [
                 _buildBox(
@@ -66,6 +66,164 @@ Widget _buildBox({required Color color, required int flex}) {
           style: TextStyle(
             color: Colors.white,
             fontSize: 25,
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+*
+import 'package:flutter/material.dart';
+
+class ExpandedAssignment extends StatelessWidget {
+  const ExpandedAssignment({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expanded Assignment'),
+      ),
+      body: Column(
+        children: [
+          _buildBox(
+            number: 8,
+            color: Colors.green,
+          ),
+          Expanded(
+            flex: 5,
+            child: Row(
+              children: [
+                _buildBox(
+                  number: 5,
+                  color: Colors.blue,
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              children: [
+                                _buildBox(
+                                  number: 1,
+                                  color: Colors.pink,
+                                ),
+                                _buildBox(
+                                  number: 1,
+                                  color: Colors.teal,
+                                ),
+                              ],
+                            ),
+                          ),
+                          _buildBox(number: 2, color: Colors.black),
+                        ],
+                      ),
+                      _buildBox(number: 3, color: Colors.brown),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _buildBox({required int number, required Color color}) {
+  return Expanded(
+    flex: number,
+    child: Container(
+      constraints: BoxConstraints.expand(),
+      color: color,
+      child: Text(
+        '$number',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 26,
+        ),
+      ),
+    ),
+  );
+}
+
+*/
+
+import 'package:flutter/material.dart';
+
+class ExpandedAssignment extends StatelessWidget {
+  const ExpandedAssignment({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expanded Assignment'),
+      ),
+      body: Column(
+        children: [
+          _buildBox(
+            number: 8,
+            color: Colors.brown,
+          ),
+          Expanded(
+            flex: 3,
+            child: Row(
+              children: [
+                _buildBox(
+                  number: 5,
+                  color: Colors.black,
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  _buildBox(
+                                      number: 1, color: Colors.pinkAccent),
+                                  _buildBox(number: 1, color: Colors.lime),
+                                ],
+                              ),
+                            ),
+                            _buildBox(number: 2, color: Colors.teal),
+                          ],
+                        ),
+                      ),
+                      _buildBox(number: 3, color: Colors.purple),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Widget _buildBox({required int number, required Color color}) {
+  return Expanded(
+    flex: number,
+    child: Container(
+      constraints: BoxConstraints.expand(),
+      color: color,
+      child: Center(
+        child: Text(
+          '$number',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 26,
           ),
         ),
       ),
