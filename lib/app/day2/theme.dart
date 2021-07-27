6e6ba1b6928a60eae16e3868d840cbcad023d14f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-27 12:52:35 +0100

    Theming

---
+++ lib/app/day2/theme.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+var currentTheme = CustomTheme();
+
+class CustomTheme with ChangeNotifier {
+  static bool _isDarkTheme = true;
+  ThemeMode get currentTheme => _isDarkTheme ? ThemeMode.dark : ThemeMode.light;
+  void toggleTheme() {
+    _isDarkTheme = !_isDarkTheme;
+    notifyListeners();
+  }
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: Colors.lightBlue,
+      backgroundColor: Colors.white,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: TextTheme(
+        headline1: TextStyle(color: Colors.black),
+        headline2: TextStyle(color: Colors.black),
+        bodyText1: TextStyle(color: Colors.black),
+        bodyText2: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      primaryColor: Colors.black,
+      backgroundColor: Colors.grey,
+      accentColor: Colors.red,
+      scaffoldBackgroundColor: Colors.grey,
+      textTheme: TextTheme(
+        headline1: TextStyle(color: Colors.white),
+        headline2: TextStyle(color: Colors.white),
+        bodyText1: TextStyle(color: Colors.white),
+        bodyText2: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_again/route/routeGenerator.dart';
+import 'app/day2/theme.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    currentTheme.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Again',
       initialRoute: '/',
       onGenerateRoute: RouteGenerator.generateRoute,
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-      ),
+      theme: CustomTheme.lightTheme,
+      darkTheme: CustomTheme.darkTheme,
+      themeMode: currentTheme.currentTheme,
     );
   }
 }
