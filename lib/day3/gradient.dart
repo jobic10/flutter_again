c352138f9ac1a76f7facf61788975db2803a8a78
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-28 16:05:07 +0100

    RadialGradient

--- lib/day3/gradient.dart
+++ lib/day3/gradient.dart
@@ -14,21 +14,22 @@ class GradientPage extends StatelessWidget {
           height: 300,
           width: 220,
           decoration: BoxDecoration(
-            gradient: LinearGradient(
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
+            gradient: RadialGradient(
               colors: [
                 Colors.red,
                 Colors.yellow,
                 Colors.brown,
-                Colors.green,
+                Colors.green.withOpacity(0.2),
               ],
               stops: [
-                0.2,
-                0.3,
-                0.4,
-                0.5,
+                0.6,
+                0.8,
+                0.81,
+                0.8,
               ],
-              begin: Alignment.bottomLeft,
-              end: Alignment.topRight,
             ),
           ),
         ),
