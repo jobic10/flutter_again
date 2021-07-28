5ca78c2fdf0add34c8fa34fce2c0fb437b4aaa78
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-28 14:16:02 +0100

    Layout in use

--- lib/day3/layout_builder.dart
+++ lib/day3/layout_builder.dart
@@ -7,8 +7,43 @@ class LayoutBuilderScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Layout Builder')),
-      body: Container(
-        color: Colors.grey,
+      body: Center(
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) => Row(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: constraints.maxHeight * .2,
+                    width: constraints.maxWidth * .5,
+                    color: Colors.lightBlue,
+                    child: Center(
+                      child: Text(
+                        'Box 1',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: constraints.maxHeight * .2,
+                    width: constraints.maxWidth * .5,
+                    color: Colors.lime,
+                    child: Center(child: Text('Box 2')),
+                  ),
+                ],
+              ),
+              Container(
+                height: constraints.maxHeight * .4,
+                width: constraints.maxWidth * .5,
+                color: Colors.yellow,
+                child: Center(
+                  child: Text('Box 3'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
