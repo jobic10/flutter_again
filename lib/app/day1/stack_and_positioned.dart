16d862dd10b9ec7c5c33b4dab5ce6116cb7bb669
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-28 15:29:03 +0100

    Format

--- lib/app/day1/stack_and_positioned.dart
+++ lib/app/day1/stack_and_positioned.dart
@@ -8,75 +8,77 @@ class StackAndPosition extends StatelessWidget {
   Widget build(BuildContext context) {
     print('My Size : -> ${MediaQuery.of(context).size}');
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Stack and Position Widget'),
+      appBar: AppBar(
+        title: Text('Stack and Position Widget'),
+      ),
+      body: Container(
+        color: Colors.tealAccent,
+        height: (MediaQuery.of(context).size.height / 2.sp).h,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            SizedBox(
+              width: 400.w,
+              height: 400.h,
+              child: Container(
+                color: Colors.white,
+              ),
+            ),
+            // SizedBox(
+            //   width: 300,
+            //   height: 300,
+            //   child: Container(
+            //     color: Colors.green,
+            //   ),
+            // ),
+            // SizedBox(
+            //   width: 150,
+            //   height: 150,
+            //   child: Container(
+            //     color: Colors.blue,
+            //   ),
+            // ),
+            Positioned(
+              child: Container(color: Colors.purple),
+              height: 150.h,
+              width: 150.w,
+              left: 30.w,
+              top: 30.h,
+            ),
+            Positioned(
+              child: Container(color: Colors.green),
+              height: 150.h,
+              width: 150.w,
+              right: 40.w,
+              top: 50.h,
+            ),
+            Positioned(
+              child: Container(color: Colors.blue),
+              height: 150.h,
+              width: 150.w,
+              left: 50.w,
+              bottom: 80.h,
+            ),
+            Positioned(
+              child: Container(
+                color: Color(0xFFFF0000),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    color: Colors.brown,
+                    height: 50.h,
+                    width: 50.w,
+                  ),
+                ),
+              ),
+              height: 150.h,
+              width: 150.w,
+              right: 30.w,
+              bottom: 60.h,
+            ),
+          ],
         ),
-        body: Container(
-            color: Colors.tealAccent,
-            height: (MediaQuery.of(context).size.height / 2.sp).h,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                SizedBox(
-                  width: 400.w,
-                  height: 400.h,
-                  child: Container(
-                    color: Colors.white,
-                  ),
-                ),
-                // SizedBox(
-                //   width: 300,
-                //   height: 300,
-                //   child: Container(
-                //     color: Colors.green,
-                //   ),
-                // ),
-                // SizedBox(
-                //   width: 150,
-                //   height: 150,
-                //   child: Container(
-                //     color: Colors.blue,
-                //   ),
-                // ),
-                Positioned(
-                  child: Container(color: Colors.purple),
-                  height: 150.h,
-                  width: 150.w,
-                  left: 30.w,
-                  top: 30.h,
-                ),
-                Positioned(
-                  child: Container(color: Colors.green),
-                  height: 150.h,
-                  width: 150.w,
-                  right: 40.w,
-                  top: 50.h,
-                ),
-                Positioned(
-                  child: Container(color: Colors.blue),
-                  height: 150.h,
-                  width: 150.w,
-                  left: 50.w,
-                  bottom: 80.h,
-                ),
-                Positioned(
-                  child: Container(
-                    color: Color(0xFFFF0000),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        color: Colors.brown,
-                        height: 50.h,
-                        width: 50.w,
-                      ),
-                    ),
-                  ),
-                  height: 150.h,
-                  width: 150.w,
-                  right: 30.w,
-                  bottom: 60.h,
-                ),
-              ],
-            )));
+      ),
+    );
   }
 }
