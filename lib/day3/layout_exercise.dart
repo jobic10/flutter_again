c0d9f21c51aeb78b3e7f8eb813ac843767652ac6
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-28 14:51:16 +0100

    Exercise paused

--- lib/day3/layout_exercise.dart
+++ lib/day3/layout_exercise.dart
@@ -6,9 +6,29 @@ class LayoutBuilderExercise extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Exercise'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Exercise'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(5),
+          child: GridView.count(
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+            crossAxisCount: 2,
+            children: List.generate(
+              30,
+              (index) => Container(
+                color: Colors.red.withAlpha(50 * index % 255),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    '$index',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
