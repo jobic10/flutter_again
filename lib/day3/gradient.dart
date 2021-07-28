81c4e019382a8f26c2116ad31f3dc94369f3bd88
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-28 15:57:07 +0100

    Gradient Stops

--- lib/day3/gradient.dart
+++ lib/day3/gradient.dart
@@ -6,17 +6,33 @@ class GradientPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Gradient'),
-        ),
-        body: Container(
-          height: 100,
-          width: 120,
+      appBar: AppBar(
+        title: Text('Gradient'),
+      ),
+      body: Center(
+        child: Container(
+          height: 300,
+          width: 220,
           decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Colors.red,
-            Colors.yellow,
-          ])),
-        ));
+            gradient: LinearGradient(
+              colors: [
+                Colors.red,
+                Colors.yellow,
+                Colors.brown,
+                Colors.green,
+              ],
+              stops: [
+                0.2,
+                0.3,
+                0.4,
+                0.5,
+              ],
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
