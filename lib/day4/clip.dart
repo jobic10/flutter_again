8881315c6cd41b99e3f95d2038c59cd39849b275
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-31 15:32:51 +0100

    Curve

--- lib/day4/clip.dart
+++ lib/day4/clip.dart
@@ -5,6 +5,50 @@ class MyClip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Clip')), body: Container());
+    return Scaffold(
+      appBar: AppBar(title: Text('Clip')),
+      body: Container(
+          child: Stack(
+        children: [
+          Opacity(
+            child: ClipPath(
+              child: Container(
+                height: 300,
+                width: double.infinity,
+                color: Colors.deepOrangeAccent,
+              ),
+              clipper: WaveClipper(),
+            ),
+            opacity: 0.5,
+          ),
+        ],
+      )),
+    );
+  }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    debugPrint(size.toString());
+    var path = Path();
+    path.lineTo(0, size.height);
+    var firstStart = Offset(size.width / 5, size.height);
+    var firstEnd = Offset(size.width / 2.25, size.height - 50);
+    path.quadraticBezierTo(
+        firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
+    var secondStart =
+        Offset(size.width - (size.width / 3.24), size.height - 105);
+    var secondEnd = Offset(size.width, size.height - 10);
+    path.quadraticBezierTo(
+        secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
