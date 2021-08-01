9f3ba7d0a84efaf7c667eac8679233e59b25b530
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-01 23:44:20 +0100

    🔥

--- lib/day4/clip.dart
+++ lib/day4/clip.dart
@@ -44,16 +44,32 @@ class WaveClipper extends CustomClipper<Path> {
   Path getClip(Size size) {
     // size = Size(size.width - 100, size.height - 100);
     var path = Path();
-    path.lineTo(0, size.height * .7);
-    Offset firstControlPoint = Offset(size.width * .3, size.height * .58);
-    Offset firstEndPoint = Offset(size.width * .5, size.height * .7);
-    Offset secondControlPoint = Offset(size.width * .8, size.height * .88);
-    Offset secondEndPoint = Offset(size.width, size.height * .7);
+    final upHeight = size.height * .7;
+    final downHeight = size.height * .8;
+    path.lineTo(0, downHeight);
+    // First Controls
+    var firstControlPoint = Offset(size.width * .2, upHeight);
+    var firstEndPoint = Offset(size.width * .33, downHeight);
+
+    // Second Controls
+    var secondControlPoint = Offset(size.width * .5, upHeight);
+    var secondEndPoint = Offset(size.width * .66, downHeight);
+    // Third Controls
+    var thirdControlPoint = Offset(size.width * .8, upHeight);
+    var thirdEndPoint = Offset(size.width, downHeight);
+
+    // Fourth Controls
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
         firstEndPoint.dx, firstEndPoint.dy);
 
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondEndPoint.dx, secondEndPoint.dy);
+
+    path.quadraticBezierTo(thirdControlPoint.dx, thirdControlPoint.dy,
+        thirdEndPoint.dx, thirdEndPoint.dy);
+
+    //   path.quadraticBezierTo(fourthControlPoint.dx, fourthControlPoint.dy,
+    // fourthEndPoint.dx, fourthEndPoint.dy);
 
     path.lineTo(size.width, 0);
     path.close();
