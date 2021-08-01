1cde3aa7e246a02a662d94e41c22280c45932936
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-01 23:08:49 +0100

    refactor

--- lib/day4/clip.dart
+++ lib/day4/clip.dart
@@ -33,25 +33,6 @@ class WaveClipper extends CustomClipper<Path> {
   Path getClip(Size size) {
     // size = Size(size.width - 100, size.height - 100);
     var path = Path();
-    path.lineTo(0, size.height - 80);
-    var firstControlPoint = Offset(size.width / 4, size.height);
-    var firstEndPoint = Offset(size.width / 2.25, size.height - 30);
-    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
-        firstEndPoint.dx, firstEndPoint.dy);
-    print(
-        "First Control Point ->  : $firstControlPoint and Fist End Point is $firstEndPoint");
-    // var firstControlPoint = Offset(size.width / 4, size.height / 3);
-    // path.lineTo(size.width, size.height / 2);
-    var secondControlPoint =
-        Offset(size.width - size.width / 3.25, size.height - 65);
-    var secondEndPoint = Offset(size.width, size.height - 40);
-    print(
-        "Second Control Point ->  : $secondControlPoint and Second End Point is $secondEndPoint");
-
-    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
-        secondEndPoint.dx, secondEndPoint.dy);
-    print('Line to ${size.width} and y is ${size.height}');
-    print('Line to ${size.width} and y is ${size.height - 40}');
 
     path.lineTo(size.width, 0);
     path.close();
@@ -59,7 +40,6 @@ class WaveClipper extends CustomClipper<Path> {
   }
 
   @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return true; //Change back to false
-  }
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) =>
+      true; //Change back to false
 }
